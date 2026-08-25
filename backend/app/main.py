f47b261c64684b6cdde8eddb1c5bcf8486262b5d
Srujal <srujalsau@gmail.com>
2026-08-25 15:41:20 +0530

    feat: complete Part 5 Docker and Docker Compose containerization

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Enable CORS for React frontend (Vite default port is 5173)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173",  "http://localhost:3000", "http://127.0.0.1:3000",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
